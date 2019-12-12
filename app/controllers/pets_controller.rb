08c65580c47584b10ae972f90a207fafe3c251fc
Tam Vu <vutam_a@hotmail.com>
2019-12-12 02:38:35 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -44,5 +44,10 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
   
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
   
 end
