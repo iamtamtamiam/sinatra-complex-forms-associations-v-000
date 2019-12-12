5a06c2fc36e9630f5490ef747e79c5eae9b8cbe3
Tam Vu <vutam_a@hotmail.com>
2019-12-12 02:30:14 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,11 +5,20 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    
+    if params[:owner_name] == ""
+     owner_object = Owner.find_by(id: params[:owner][:id])
+   else
+     owner_object = Owner.create(name: params[:owner_name])
+   end
+
+   @pet = Pet.create(name: params[:pet_name], owner: owner_object)
 
     redirect to "pets/#{@pet.id}"
   end
